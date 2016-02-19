1a1803fa36bd4be339ffd8d730b40b7044bca423
Ross Patman <ross.patman@vml.com>
2016-02-19 15:38:37 +0000

    Add test

--- JenkinsTestTests/JenkinsTestTests.swift
+++ JenkinsTestTests/JenkinsTestTests.swift
@@ -21,9 +21,8 @@ class JenkinsTestTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testPass() {
+        XCTAssertTrue(true)
     }
     
     func testPerformanceExample() {
